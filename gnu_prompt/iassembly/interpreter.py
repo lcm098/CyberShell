af43870bd19918df4a9eb3740550fb32a0355c5a
lcm098 <danishksinha@gmail.com>
2025-03-21 20:40:01 +0530

    Working

--- gnu_prompt/iassembly/interpreter.py
+++ gnu_prompt/iassembly/interpreter.py
@@ -124,7 +124,7 @@ class Interpreter(ExprVisitor):
         
     def is_opponent_y_regis(self, y, line):
         
-        if (y[1] == "register" or y[1] == "identifier"):
+        if y[1] in ("register", "identifier"):
             if self.environment.is_defined(y):
                 value = self.environment.get(y)
                 if value[1] == "register":

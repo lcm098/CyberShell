7d589fac2b65dc5c979abccf9d94f50ac08515a2
lcm098 <danishksinha@gmail.com>
2025-03-23 16:31:02 +0530

    Working

--- gnu_prompt/iassembly/STANDER_LIB/read.py
+++ gnu_prompt/iassembly/STANDER_LIB/read.py
@@ -1,22 +1,16 @@
-from iassembly.environment import environment
 
 class Read:
     
     def __init__(self, args):
         self.args = args
-        self.environment = environment
         
     def read_it(self):
         
         buff = []
         
         for item in self.args:
-             
-            temp = input()
-            if self.environment.is_defined((item, item, id(item))):
-                self.environment.assign((item, item, id(item)), (temp, type(temp).__name__, id(temp)))
-            else:
-                self.environment.define((item, item, id(item)), (temp, type(temp).__name__, id(temp)), False)
-            buff.append((item, type(item).__name__, id(item)))
+            if int(item) == 0: 
+                user_input = input()
+                buff.append((user_input, type(user_input).__name__, id(user_input)))
             
         return buff
